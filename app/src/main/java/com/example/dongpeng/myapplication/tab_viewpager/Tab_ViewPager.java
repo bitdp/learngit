bede46870d119ad5ce1163ebfbabafa1eee9feb4
dongpeng <457055340@163.com>
2016-08-19 15:25:05 +0800

    TabLayout跟ViewPager联用

---
+++ app/src/main/java/com/example/dongpeng/myapplication/tab_viewpager/Tab_ViewPager.java
@@ -0,0 +1,9 @@
+package com.example.dongpeng.myapplication.tab_viewpager;
+
+import android.app.Activity;
+
+/**
+ * Created by dongpeng on 2016/8/19.
+ */
+public class Tab_ViewPager extends Activity {
+}
