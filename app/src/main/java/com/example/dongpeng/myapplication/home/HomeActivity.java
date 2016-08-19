2b19e680e502fe2e5c4f353ada7e1c938be74466
douhaoqiang <douhaoqiang@126.com>
2016-08-19 17:22:57 +0800

    创建base文件

---
+++ app/src/main/java/com/example/dongpeng/myapplication/home/HomeActivity.java
@@ -0,0 +1,10 @@
+package com.example.dongpeng.myapplication.home;
+
+import android.app.Activity;
+
+/**
+ * Created by douhaoqiang on 2016/8/19.
+ */
+
+public class HomeActivity extends Activity {
+}
