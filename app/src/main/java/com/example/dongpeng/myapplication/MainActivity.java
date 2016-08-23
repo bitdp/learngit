92ad9749784536869642c7f7b630f75248380479
dongpeng <457055340@163.com>
2016-08-23 16:03:29 +0800

    sss

--- app/src/main/java/com/example/dongpeng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dongpeng/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.dongpeng.myapplication.fresco.FrescoActivity;
 import com.example.dongpeng.myapplication.rxjava.activity.RxBusActivity;
 import com.example.dongpeng.myapplication.tab_viewpager.Tab_ViewPager_Activity;
 
@@ -18,6 +19,8 @@ public class MainActivity extends Activity {
     Button button_tab;
     @BindView(R.id.button_rx)
     Button buttonRx;
+    @BindView(R.id.button_fresco)
+    Button buttonFresco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +30,20 @@ public class MainActivity extends Activity {
     }
 
 
-    @OnClick({R.id.button_tab, R.id.button_rx})
+    @OnClick({R.id.button_tab, R.id.button_rx, R.id.button_fresco})
     public void onClick(View view) {
         Intent intent;
         switch (view.getId()) {
             case R.id.button_tab:
-                 intent = new Intent(MainActivity.this, Tab_ViewPager_Activity.class);
+                intent = new Intent(MainActivity.this, Tab_ViewPager_Activity.class);
                 startActivity(intent);
                 break;
             case R.id.button_rx:
-                 intent = new Intent(MainActivity.this, RxBusActivity.class);
+                intent = new Intent(MainActivity.this, RxBusActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_fresco:
+                intent = new Intent(MainActivity.this, FrescoActivity.class);
                 startActivity(intent);
                 break;
         }
